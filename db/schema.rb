92312155bb42764d615d3d15536b190b60276b5f
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-03-11 22:25:26 +0000

    8° commit - Iniciando tdd do recurso users da API

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_11_214542) do
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
