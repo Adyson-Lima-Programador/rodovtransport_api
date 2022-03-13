561fa37c902780b3e11328e2b88d4309a39d4dd5
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-03-13 16:17:51 +0000

    19° commit - DELETE /users/{id} green

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -42,4 +42,30 @@ RSpec.describe "Users", type: :request do
     
   end
   
+  describe "PATCH /users/{id}" do
+    
+    it "Consegue atualizar um usuário -> status 200 ok" do
+      headers = {"ACCEPT" => "application/json"}
+      user = User.first
+      user.name += " - Atualizado" # mesmo primeiro user com nome atualizado
+      patch "/users/#{user.id}.json", params: {user: user.attributes}, headers: headers
+      expect(response.body).to include_json(
+        id: /\d/,
+        name: user.name,) 
+      expect(response).to have_http_status(200)
+    end
+    
+  end
+  
+  describe "DELETE /users/{id}" do
+    
+    it "Consegue apagar um usuário -> status 204 no content" do
+      headers = {"ACCEPT" => "application/json"}
+      user = User.first
+      expect {delete "/users/#{user.id}.json", headers: headers}.to change(User, :count).by(-1)
+      expect(response).to have_http_status(204)
+    end
+    
+  end
+  
 end
