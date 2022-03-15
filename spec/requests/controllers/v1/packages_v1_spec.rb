1c86e4fc72b2f95775129bf7a57440d388d567e5
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-03-15 12:14:10 +0000

    40° commit - GET /packages V1 green

---
+++ spec/requests/controllers/v1/packages_v1_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe V1::PackagesController, type: :controller do
+  
+  before { @packages = create_list(:package, 3) }
+  
+  describe "GET /packages" do
+    
+    it "Consegue listar todos os pacotes -> status 200 ok" do
+      get :index
+      expect(JSON.parse(response.body).size).to eq(3)
+      expect(response).to have_http_status(200)
+    end
+    
+  end
+  
+  
+  
+end
