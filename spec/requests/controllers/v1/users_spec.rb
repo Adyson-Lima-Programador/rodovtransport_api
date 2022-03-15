539b2caf49c50b0e1e6d9e778f738c30278129dd
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-03-15 11:21:24 +0000

    28° commit - refatorando teste GET /users green para API versionada em V1 e V2

---
+++ spec/requests/controllers/v1/users_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe V1::UsersController, type: :controller do
+  
+  before { @users = create_list(:user, 3) }
+  
+  describe "GET /users" do
+    
+    it "Consegue listar todos os usuários -> status 200 ok" do
+      get :index
+      expect(JSON.parse(response.body).size).to eq(3)
+      expect(response).to have_http_status(200)
+    end
+    
+  end
+  
+end
