6c18be9262d6b6c632371e56d67a454f3bbabf10
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-03-15 12:19:36 +0000

    41° commit - GET /packages/{id} V1 green

--- spec/requests/controllers/v1/packages_v1_spec.rb
+++ spec/requests/controllers/v1/packages_v1_spec.rb
@@ -14,6 +14,19 @@ RSpec.describe V1::PackagesController, type: :controller do
     
   end
   
+  describe "GET /packages/{id}" do
+    
+    it "Consegue listar um pacote especifico -> status 200 ok" do
+      get :show, params: {id: @packages[0].id}
+      expect(response.body).to include_json(
+        id: @packages[0].id,
+        content: @packages[0].content,
+        status: @packages[0].status)
+      expect(response).to have_http_status(200)
+    end
+    
+  end
+  
   
   
 end
