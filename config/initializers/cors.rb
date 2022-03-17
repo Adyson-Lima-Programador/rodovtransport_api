bb109519571f5ed02394d60a29af176e07515642
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-03-17 16:09:10 +0000

    58° commit - configurado setup para heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost"# Configurar urls de origem permitidos aqui
+    origins "*"# Configurar urls de origem permitidos aqui
 
     resource "*",
       headers: :any,
