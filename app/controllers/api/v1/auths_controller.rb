58ae5fd92917d66e892bd74768f66172b5c1a85e
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-03-28 21:32:08 +0000

    62° commit - seeds refatorado

--- app/controllers/api/v1/auths_controller.rb
+++ app/controllers/api/v1/auths_controller.rb
@@ -2,20 +2,25 @@
   class Api::V1::AuthsController < ApplicationController
     
     def create
+      # define chave JWT
       hmac_secret = '123'
+      
+      # prepara payload com dados vindos do frontend
       payload = {email: params[:email]}, {password: params[:password]}
       
       # payload = {email: 'admin@admin.com'}, {password: '123'} # descomentando, sempre autentica ;)
+      
+      # cria token
       token = JWT.encode payload, hmac_secret, 'HS256'
       
-      # pesquisa se email vindo do Angular existe no banco de dados
+      # pesquisa se email vindo do frontend existe no banco de dados
       @user = User.find_by(email: params[:email]) 
       
       # só renderiza token se o usuário estiver cadastrado no banco de dados
       if(@user != nil)
         
         if(@user.email == params[:email] and @user.password == params[:password])
-          render json: {token: token}
+          render json: {categoria: @user.category, token: token}
         else
           render json: {erro: "senha incorreta"}
         end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 require 'faker'
 
-puts 'Gerando clientes...'
-User.create! email: 'user@user.com', name: 'user', password: "123", category: 'cliente'
-User.create! email: 'admin@admin.com', name: 'admin', password: "123", category: 'transportadora'
+puts 'Gerando clientes... <categoria 1 = transportadora/admin, categoria 2 = user >'
+User.create! email: 'admin@admin.com', name: 'admin', password: "123", category: '1'
+User.create! email: 'user@user.com', name: 'user', password: "123", category: '2'
 98.times do |i|
-  User.create! email: Faker::Internet.email, name: Faker::Name.name, password: "123", category: ["cliente","transportadora"].sample
+  User.create! email: Faker::Internet.email, name: Faker::Name.name, password: "123", category: ["1","2"].sample
 end
 puts 'Clientes gerados com sucesso!'
 
